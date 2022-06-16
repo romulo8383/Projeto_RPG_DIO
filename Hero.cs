26b6513dc627c0ad4039fccf58d75bdb22b058d5
romulo8383 <romulo8383@gmail.com>
2022-06-16 18:32:26 -0300

    atualização classe abstract Hero

--- Hero.cs
+++ Hero.cs
@@ -5,8 +5,32 @@ using System.Threading.Tasks;
 
 namespace Projeto_RPG_DIO
 {
-    public class Hero
+    public abstract class Hero
     {
-        
+        public Hero(string Name, int Level, string Herotype)
+        {
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = Herotype;
+         
+        }
+        public Hero(){
+
+        }
+        public string Name;
+        public int Level;
+        public string HeroType;
+
+        public override string ToString()
+        {
+            return this.Name + " " + this.Level + " " + this.HeroType;
+
+        }
+
+        public virtual string Attack()
+        {
+            return this.Name+ " Atacou ";
+        }
     }
+    
 }
