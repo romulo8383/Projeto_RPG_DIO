28971f57a936f04514e312f2592dae2bac7efb81
romulo8383 <romulo8383@gmail.com>
2022-06-16 19:19:06 -0300

    criação da função Main

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Projeto_RPG_DIO.src.entities
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Knight arus = new Knight("Arus", 52, "Knight");
+            Wizard wizard = new Wizard("Jenica", 48,"White Wizard");
+            Ninja weder = new Ninja("weder", 40, "Ninja");
+            BlackWizard thron = new BlackWizard("Thron", 35, "Black Wizard");
+
+            Console.WriteLine(wizard.Attack(7));
+            Console.WriteLine(arus.Attack(2));
+            Console.WriteLine(weder.Attack(7));
+            Console.WriteLine(thron.Attack(1));
+            
+        }
+    }
+}
